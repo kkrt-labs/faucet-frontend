a1d8b51d1bc99bde52c6c189f4f2b86a51aefe44
parzuko <jivansh17@gmail.com>
2024-05-18 00:04:27 +0530

    :recycle: update client to use secret

--- lib/thirdweb-client.ts
+++ lib/thirdweb-client.ts
@@ -3,13 +3,18 @@ import { createWallet, walletConnect } from "thirdweb/wallets";
 
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
+const secretKey = process.env.THIRDWEB_SECRET_KEY;
 
 if (!clientId) {
   throw new Error("No client ID provided");
 }
 
-export const client = createThirdwebClient({
-  clientId: clientId,
-});
+export const client = createThirdwebClient(
+  secretKey
+    ? { secretKey }
+    : {
+        clientId,
+      }
+);
 
 export const wallets = [createWallet("io.metamask"), walletConnect(), createWallet("me.rainbow")];
