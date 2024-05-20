f60b1f93a684e45a70763993f703c0c8a29802a7
parzuko <jivansh17@gmail.com>
2024-05-21 00:46:27 +0530

    :truck: add components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,8 +10,9 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        main: "primaryButton text-white font-normal",
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground font-normal",
         secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
@@ -19,7 +20,7 @@ const buttonVariants = cva(
         wallet: "rounded-[6px] bg-gradient-to-r from-[#F54400] to-[#F54400] text-white shadow-sm hover:bg-secondary/80",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-10 px-4 py-3",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
