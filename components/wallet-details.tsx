e84b9008ef322f85f46a306377a29083109b00b8
parzuko <jivansh17@gmail.com>
2024-05-21 00:46:47 +0530

    :lipstick: details style

--- components/wallet-details.tsx
+++ components/wallet-details.tsx
@@ -19,7 +19,7 @@ export const WalletDetails = () => {
         background: "linear-gradient(0deg, #5E5E5E 0%, #5E5E5E 100%), rgba(255, 255, 255, 0.40)",
         boxShadow: "0px 1px 2px 0px rgba(164, 172, 185, 0.24), 0px 0px 0px 1px rgba(18, 55, 105, 0.08)",
       }}
-      className="space-x-2 font-medium"
+      className="space-x-2 font-normal"
       variant="wallet"
       size="withIcon"
     >
