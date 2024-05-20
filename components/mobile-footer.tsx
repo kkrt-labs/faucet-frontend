269206e460c7736b43e8ce81f68afcf60af58564
parzuko <jivansh17@gmail.com>
2024-05-21 01:54:47 +0530

    :recycle: use new button

--- components/mobile-footer.tsx
+++ components/mobile-footer.tsx
@@ -5,15 +5,15 @@ import { Button } from "./ui/button";
 
 export const Footer = () => {
   return (
-    <footer className="absolute bottom-4 flex flex-col space-y-6 sm:hidden">
+    <footer className="absolute bottom-4 flex flex-col items-center space-y-6 sm:hidden">
       <div className="flex flex-row items-center space-x-6">
         <IconLink src="/assets/x-icon.svg" href="https://x.com" name="X" />
         <IconLink src="/assets/discord-icon.svg" href="https://discord.com" name="Discord" />
         <IconLink src="/assets/farcaster-icon.svg" href="https://farcaster.xyz/" name="Farcaster" />
       </div>
       <Link href="/">
-        <Button className="space-x-2" variant="reverse" size="withIcon">
-          <span>Read Docs</span>
+        <Button className="space-x-2" variant="outline" size="withIcon">
+          <span className="text-[#ff4500]">Read Docs</span>
           <Image src="/assets/docs-icon.svg" alt="Docs" width={16} height={16} />
         </Button>
       </Link>
