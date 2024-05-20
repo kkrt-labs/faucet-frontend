6854e9c7f079114a9d8b4fe64485de14d8428e12
parzuko <jivansh17@gmail.com>
2024-05-21 01:54:36 +0530

    :lipstick: button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,6 +11,8 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         main: "primaryButton text-white font-normal",
+        success: "successButton text-white font-normal",
+        fail: "failButton text-white font-normal",
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground font-normal",
         secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
