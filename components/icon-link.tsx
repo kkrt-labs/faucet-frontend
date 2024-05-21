a20bad41ce8c2a2efdb31f3acc6b43d53d12ca05
parzuko <jivansh17@gmail.com>
2024-05-22 00:48:37 +0530

    :recycle: open links in new tab

--- components/icon-link.tsx
+++ components/icon-link.tsx
@@ -9,7 +9,7 @@ interface IconLinkProps {
 
 export const IconLink = ({ src, href, name }: IconLinkProps) => {
   return (
-    <Link href={href}>
+    <Link rel="noopener noreferrer" target="_blank" href={href}>
       <Image src={src} alt={`${name} Logo`} width={24} height={24} priority className="w-[30px] h-6" />
     </Link>
   );
