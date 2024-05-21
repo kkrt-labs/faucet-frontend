ba6bf5901d22d840f36e94a347c44679b457c1ae
parzuko <jivansh17@gmail.com>
2024-05-21 17:30:47 +0530

    :lipstick: fix spacing

--- components/text-pair.tsx
+++ components/text-pair.tsx
@@ -4,7 +4,9 @@ export const TextPair = ({ heading, description }: { heading: string; descriptio
       <header>
         <h3 className="scroll-m-20 text-2xl tracking-tight">{heading}</h3>
       </header>
-      <p className="leading-5 [&:not(:first-child)]:mt-4 text-[#878794] text-center text-pretty">{description}</p>
+      <p className="leading-5 [&:not(:first-child)]:mt-4 text-[#878794] text-center text-pretty max-w-[400px]">
+        {description}
+      </p>
     </section>
   );
 };
