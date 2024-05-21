6dc31b04dfc92e41cd4fea6872595fdc8a6d723f
parzuko <jivansh17@gmail.com>
2024-05-22 00:13:39 +0530

    :sparkles: create 404

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { TextPair } from "@/components/text-pair";
+import image404 from "@/public/assets/404.svg";
+
+export default function FourOFour() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <Image src={image404} alt="404" className="mb-16 mt-10" />
+      <TextPair
+        heading="Need more testnet ETH?"
+        description="Reach out to us @Kakarot Discord and raise a ticket if you need large amount of testnet ETH."
+      />
+      <Button className="space-x-2 max-w-[120px] mt-6" variant="outline" size="withIcon">
+        <span>Reach Out</span>
+        <Image src="/assets/link-icon.svg" alt="Docs" width={16} height={16} />
+      </Button>
+    </main>
+  );
+}
