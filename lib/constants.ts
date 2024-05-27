64474007375e8d72aaea0f55a134d940c7f906af
parzuko <jivansh17@gmail.com>
2024-05-27 20:43:45 +0530

    🔧 update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,7 +8,7 @@ interface Environment {
 
 const ENV: Environment = {
   NODE_ENV: process.env.NODE_ENV || "development",
-  API_ROOT: process.env.API_ROOT || "",
+  API_ROOT: process.env.NEXT_PUBLIC_API_ROOT || "",
   NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "",
   THIRDWEB_CLIENT_SECRET: process.env.THIRDWEB_CLIENT_SECRET || "",
 };
