99e29ecdfafb18fbbd4ec9500572ec1fb17bdeba
parzuko <jivansh17@gmail.com>
2024-05-28 14:59:37 +0530

    :sparkles: add faucet utility

--- components/faucet.tsx
+++ components/faucet.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
-import { useState } from "react";
-import { useActiveAccount } from "thirdweb/react";
+import { useEffect, useState } from "react";
+import { useActiveAccount, useWalletBalance } from "thirdweb/react";
+import { ethereum } from "thirdweb/chains";
 
 import { TextPair } from "@/components/text-pair";
 import { Button } from "@/components/ui/button";
 import { useClaimFunds } from "@/mutations/useClaimFunds";
+import { useFaucetJob } from "@/queries/useFaucetJob";
+import { client } from "@/lib/thirdweb-client";
 
 export const Faucet = () => {
   const { mutate: claimFunds, isPending, data: claimJobID } = useClaimFunds();
+  const { data: faucetJob } = useFaucetJob(claimJobID?.jobID ?? "");
+  const [available, setAvailable] = useState(0.001);
+
   const wallet = useActiveAccount();
+  const { data: walletBalance } = useWalletBalance({ chain: ethereum, address: wallet?.address as string, client });
+
+  const isProcessing = isPending || (faucetJob && faucetJob[0].status !== "completed") || available === 0;
 
   const handleClaim = async () => {
     claimFunds({ walletAddress: wallet?.address as string });
     setAvailable(0);
   };
 
-  const [available, setAvailable] = useState(0.001);
+  useEffect(() => {
+    if (faucetJob && faucetJob[0].status === "completed") {
+      setAvailable(0.001);
+    }
+  }, [faucetJob]);
+
   return (
     <main className="flex flex-col items-center">
       <div
@@ -27,15 +41,15 @@ export const Faucet = () => {
         }}
       >
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between">
-          <DetailAndText title="Balance" text="0 Tokens" />
+          <DetailAndText title="Balance" text={`${walletBalance?.displayValue.substring(0, 6)} ETH`} />
           <DetailAndText title="Daily Allowance" text="0.001 ETH" />
           <DetailAndText title="Cooldown" text="00:52:34" />
         </div>
         <div className="flex flex-col justify-center items-center my-16 sm:my-24">
           <div className="w-full sm:w-fit text-center">
             <h2 className="text-5xl md:text-7xl leading-tight text-[#878794]">{available} ETH</h2>
-            <Button onClick={handleClaim} variant="main" className="mt-6 w-full" disabled={isPending}>
-              Claim {isPending ? "Processing..." : claimJobID?.jobID}
+            <Button onClick={handleClaim} variant="main" className="mt-6 w-full" disabled={isProcessing}>
+              {isProcessing ? "Processing..." : "Claim"}
             </Button>
           </div>
         </div>
