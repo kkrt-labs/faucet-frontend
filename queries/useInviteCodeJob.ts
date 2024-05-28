f849ef01744b8d716e604195784cefd73a46e9ca
parzuko <jivansh17@gmail.com>
2024-05-28 14:57:05 +0530

    :recycle: add polling interval

--- queries/useInviteCodeJob.ts
+++ queries/useInviteCodeJob.ts
@@ -5,10 +5,13 @@ interface InviteCodeJobParams {
   jobId: string;
 }
 
+const isComplete = (query: any) => query.state.data && query.state.data[0].status === "completed";
+
 const useInviteCodeJob = (jobId: string) => {
   return useQuery({
     queryKey: ["useInviteCodeJob", jobId],
     queryFn: () => API.jobs.redeemCode(jobId),
+    refetchInterval: (query) => (isComplete(query) ? false : 2000),
   });
 };
 
