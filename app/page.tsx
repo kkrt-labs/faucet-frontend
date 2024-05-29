b92b678f6bab1695df5c86e2afeaf916fa62535b
parzuko <jivansh17@gmail.com>
2024-05-29 21:12:08 +0530

    :memo2: housekeeping

--- app/page.tsx
+++ app/page.tsx
@@ -21,14 +21,14 @@ export default function Home() {
     }
   }, [data, isLoading]);
 
-  if (goToFaucet) return <Faucet />;
-  else if (isWhitelisted) return <FreePass shouldGoToFaucet={() => setGoToFaucet(true)} />;
+  if (wallet && goToFaucet) return <Faucet />;
+  else if (isWhitelisted && wallet) return <FreePass shouldGoToFaucet={() => setGoToFaucet(true)} />;
 
   return (
     <main className="flex flex-col justify-center items-center text-center py-12 sm:py-20">
-      <h1 className="scroll-m-20 text-4xl font-medium tracking-tight lg:text-[52px]">Welcome to Kakarot faucet</h1>
+      <h1 className="scroll-m-20 text-4xl font-medium tracking-tight lg:text-[52px]">Get free testnet Kakarot ETH</h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6 text-foreground">
-        Get test tokens for building applications on the Superchain
+        The fast, native faucet to kickstart your journey in the Kakarot ecosystem.
       </p>
       {!wallet && <IntroSplash />}
       {wallet && <InviteCodeSplash setIsWhitelisted={() => setIsWhitelisted(true)} />}
