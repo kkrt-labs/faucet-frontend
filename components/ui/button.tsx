bbd2782c416f1fb7b8674d380ee455a66dd588af
parzuko <jivansh17@gmail.com>
2024-05-29 21:10:44 +0530

    :lipstick: cooldown styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,6 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         main: "primaryButton text-white font-normal",
+        cooldown: "cooldownButton text-white font-normal",
         success: "successButton text-white font-normal",
         fail: "failButton text-white font-normal",
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
