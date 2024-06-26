bbadacffd022808205fee784dd881c61b6a7b96a
parzuko <jivansh17@gmail.com>
2024-06-27 01:09:55 +0530

    :sparkles: cache jobID

--- app/faucet/page.tsx
+++ app/faucet/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Confetti from "react-confetti";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useBlockNumber, useWalletBalance } from "thirdweb/react";
 import { toast } from "sonner";
@@ -19,15 +19,16 @@ import { FaucetSuccess } from "@/components/faucet-success";
 import { TextPair } from "@/components/text-pair";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+
+const LOCAL_STORAGE_KEY = "faucetJobId";
 
 export default function Faucet() {
   const { wallet, faucetStats, faucetBalance, refetchFaucet, isFaucetLoading } = useFaucet();
   const { width: windowWidth } = useWindowSize();
 
   const blockNumber = useBlockNumber({ client, chain: KAKAROT_SEPOLIA });
-  const router = useRouter();
   const { mutate: claimFunds, isPending, data: claimJobID } = useClaimFunds();
-  const { data: faucetJob, isError } = useFaucetJob(claimJobID?.jobID ?? "");
   const { refetch: refetchWallet } = useWalletBalance({
     chain: KAKAROT_SEPOLIA,
     address: wallet?.address as string,
@@ -36,6 +37,8 @@ export default function Faucet() {
 
   const [isProcessing, setIsProcessing] = useState(isPending);
   const [isClaimed, setIsClaimed] = useState(false);
+  const [jobId, setJobId] = useState<string | null>(null);
+  const { data: faucetJob, isError } = useFaucetJob(jobId ?? "");
 
   const available = `${faucetStats?.dripAmountInEth ?? 0.001} ETH`;
   const isCooldown = !!faucetStats && (faucetStats?.timeLeftInS !== 0 || faucetStats?.canClaim === false);
@@ -56,7 +59,7 @@ export default function Faucet() {
         </div>
         <span className="h-20 w-[2px] bg-slate-100 -my-4"></span>
         <a
-          className="text-[#f54400] flex flex-row items-center space-x-2 text-nowrap p-2"
+          className="text-kkrtOrange flex flex-row items-center space-x-2 text-nowrap p-2"
           href={`${KKRT_EXPLORER}/tx/${txHash}`}
           target="_blank"
           rel="noopener noreferrer"
@@ -68,7 +71,32 @@ export default function Faucet() {
     );
 
   useEffect(() => {
+    if (claimJobID) {
+      const currentJobId = claimJobID.jobID;
+      localStorage.setItem(LOCAL_STORAGE_KEY, currentJobId);
+      setJobId(currentJobId);
+    }
+  }, [claimJobID]);
+
+  useEffect(() => {
+    const savedJobId = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedJobId && faucetJob) {
+      const createdAt = new Date(faucetJob[0].created_at);
+      const now = new Date();
+      const diff = (now.getTime() - createdAt.getTime()) / 1000; // difference in seconds
+
+      // 300 seconds === 5 minutes
+      if (diff > 300) {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+      } else {
+        setJobId(savedJobId);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     if (faucetJob && faucetJob[0].status === "completed") {
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
       runSuccessToast(faucetJob[0].transaction_hash);
       setIsProcessing(false);
       setIsClaimed(true);
@@ -84,12 +112,12 @@ export default function Faucet() {
     }
   }, [isError]);
 
-  if (!wallet) router.replace("/");
   if (isFaucetLoading) return <SkeletonLoader />;
+  if (!wallet) redirect("/");
 
   return (
     <main className="flex flex-col items-center mt-10 h-full">
-      <div className="flex flex-col bg-white w-full py-6 px-3 sm:px-10 lg:px-20 rounded-md mb-12">
+      <div className="flex flex-col bg-white w-full py-6 px-3 sm:px-10 lg:px-20 rounded-md mb-12 border border-[#2e2e342e]">
         <Confetti
           colors={CONFETTI_COLORS}
           run={isClaimed}
@@ -100,7 +128,7 @@ export default function Faucet() {
         />
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between">
           <DetailAndText title="Faucet Balance" text={`${faucetBalance?.faucetBalanceInEth.substring(0, 6) ?? 0}ETH`} />
-          <DetailAndText title="Block Number" text={blockNumber?.toString() ?? "0x"} />
+          <DetailAndText title="Block Number" text={blockNumber?.toString() ?? "0x"} isBlock />
         </div>
         {isClaimed ? (
           <FaucetSuccess
@@ -117,6 +145,7 @@ export default function Faucet() {
             available={available}
             handleClaim={handleClaim}
             faucetStats={faucetStats}
+            faucetJob={faucetJob}
           />
         )}
       </div>
@@ -134,9 +163,9 @@ export default function Faucet() {
   );
 }
 
-const DetailAndText = ({ title, text }: { title: string; text: string }) => (
+const DetailAndText = ({ title, text, isBlock = false }: { title: string; text: string; isBlock?: boolean }) => (
   <h4 className="text-[#878794] faucetDetails px-3 py-2 rounded-sm">
-    {title}: <span className="text-[#f54400]">{text}</span>
+    {title}: <span className={cn(isBlock ? "text-[#0A846C]" : "text-kkrtOrange", "font-medium")}>{text}</span>
   </h4>
 );
 
