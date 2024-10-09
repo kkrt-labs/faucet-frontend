85a12fbe5bbf58dac55feff21ba4f1abae44ca2a
parzuko <jivansh17@gmail.com>
2024-10-09 20:12:08 +0530

    :recycle: navigation logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useQueryClient } from "@tanstack/react-query";
-import { redirect } from "next/navigation";
 import { useFaucet } from "@/hooks/useFaucet";
 import { ConnectWallet } from "@/components/connect-wallet";
-import { SkeletonLoading } from "@/components/skeleton-loading";
 import { useIsEligible } from "@/queries/useIsEligible";
 import { KakarotOG } from "@/components/kakarot-og";
 import { useEffect } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const { isFaucetLoading, wallet, activeWallets } = useFaucet();
-  const { data: isEligible, isLoading: isEligibleLoading } = useIsEligible(wallet?.address ?? "");
+  const { wallet, activeWallets } = useFaucet();
+  const { data: isEligible } = useIsEligible(wallet?.address ?? "");
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -19,18 +19,25 @@ export default function Home() {
     }
   }, [isEligible, wallet?.address, queryClient]);
 
-  if (isFaucetLoading || isEligibleLoading) return <SkeletonLoading />;
-  if (wallet && activeWallets && !isEligible?.isEligible) redirect("/faucet");
+  // if (isFaucetLoading || isEligibleLoading) return <SkeletonLoading />;
+  // if (wallet && activeWallets && !isEligible?.isEligible) redirect("/faucet");
+  if (isEligible?.isEligible) return <KakarotOG />;
 
-  return isEligible ? (
-    <KakarotOG />
-  ) : (
+  return (
     <main className="flex flex-col items-center text-center my-20 h-full">
       <h1 className="scroll-m-20 text-4xl font-medium tracking-tight lg:text-[52px]">Get free testnet Kakarot ETH</h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6 text-foreground mb-14">
         The fast, native faucet to kickstart your journey in the Kakarot ecosystem.
       </p>
-      <ConnectWallet />
+      {wallet && activeWallets ? (
+        <Link href={"/faucet"} className="w-full max-w-[400px]">
+          <Button variant="main" className="mt-6 w-full">
+            Go to Faucet
+          </Button>
+        </Link>
+      ) : (
+        <ConnectWallet />
+      )}
     </main>
   );
 }
